f6dcb7e9e0163b76e3d7c1668aad3ccbab0975bd
emmel3301 <emmanuelt@gmx.de>
2020-02-28 19:23:02 +0300

    s

--- app/Http/Controllers/Api/Location/LocationController.php
+++ app/Http/Controllers/Api/Location/LocationController.php
@@ -15,11 +15,11 @@ class LocationController extends Controller
         $loginUser= $this->getAuthUser($request);
 		$userid= $loginUser->userid;
 		$userLocation = DB::select('select * from user_location where userid = ?', [$userid]);
-		if ($userLocation == 0) {
+		//if ($userLocation == 0) {
 		$obj=new ResponseModel("No entrys.",$userLocation,1,null);
         return response()->json($obj);	
-		}
-		if ($userLocation != 0) {
+		/}
+		/*if ($userLocation != 0) {
         $userLocation->date=now();
 		$userLocation->lat = $request->lat;
         $userLocation->long1 = $request->long1;
@@ -28,7 +28,7 @@ class LocationController extends Controller
 
         $obj=new ResponseModel("Successfully updated.",$userLocation,1,null);
         return response()->json($obj);
-		}
+		}*/
     }
 
     public function getAuthUser(Request $request)
