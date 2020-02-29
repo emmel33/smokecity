4f4e22fb50720b1f6a2c34050b07102233697c70
emmel3301 <emmanuelt@gmx.de>
2020-02-29 17:39:51 +0300

    timout fertig benachrichtung

--- app/Http/Controllers/Api/Location/LocationController.php
+++ app/Http/Controllers/Api/Location/LocationController.php
@@ -44,23 +44,19 @@ class LocationController extends Controller
 		$client->injectGuzzleHttpClient(new \GuzzleHttp\Client());
 		
 		$message = new Message();
-		$message->setPriority('high');
+		$message->setPriority('normal');
 		$message->addRecipient(new Device('dIkEmJb0Hgw:APA91bFDGltZh5fDetlKOpZp4quZP9YNlmpyj2LHQeFEnzZ1nQtHc5HTcvhh5rxY8mRptGCbfsutUf1QBV0rSP_GTpQwaG8zK9SxV2rhJUMxLVRpgzHmtNgoioV65h_0rGLxcZdN1Stu'));
 		$message->addRecipient(new Device('dEc7UCC_9MA:APA91bHJrEg1GoCvRDrIH2AeLRaSVjfKazqkwZrXq23ROtd9REJzUf1MIuHSPAiCpMTtS3285BAvNL8GxArh1hM2FQrBSqk6EFCFNN0A5BEW2ArsryWvH7HtHQOSjTRA2pWV52-0rUTV'));
 		//$message->addRecipient(new Device('_YOUR_DEVICE_TOKEN_3_'));
 		$message
 			->setNotification(new Notification('Hallo', 'Nachricht'))
 			->setData(['key' => 'value']);
-		//$log->debug("Serialized message before adding timeouts: " . json_encode($message));
-		$messagetimeout = "60";
+	
+		$messagetimeout = "1800"; //zeit wie lange benachrichtung gespeichert wird
 		$message->setJsonKey("apns", ["headers" => ["apns-expiration" => time() + $messagetimeout]]);
 		$message->setJsonKey("android", ["ttl" => $messagetimeout . "s"]);
 		$message->setJsonKey("webpush", ["headers" => ["TTL" => $messagetimeout . ""]]);
-		//$log->debug("Serialized message after adding timeouts: " . json_encode($message));
-			
 		$response = $client->send($message);
-		//var_dump($response->getStatusCode());
-		//var_dump($response->getBody()->getContents());
 		
 		
 		$obj=new ResponseModel("Successfully updated.",$userLocation,1,null);
